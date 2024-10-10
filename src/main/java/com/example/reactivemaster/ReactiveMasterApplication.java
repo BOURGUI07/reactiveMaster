929cc59195cae7f47b347fe74816cefacbbf87ed
Youness Bourgui <younessbourgui07@gmail.com>
2024-10-10 10:23:15 +0200

    Project setup

---
+++ src/main/java/com/example/reactivemaster/ReactiveMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivemaster/ReactiveMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
